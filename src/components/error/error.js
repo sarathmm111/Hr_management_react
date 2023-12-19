3ef7d3a6fc49a90c87b110aa9a5139fb3501150d
sarath <sarathmm224@gmail.com>
2023-12-19 19:11:09 +0530

    add error component

---
+++ src/components/error/error.js
@@ -0,0 +1,14 @@
+// Error.js
+
+import React from 'react';
+import './error.css';
+
+const Error = () => {
+  return (
+    <div className="error-container">
+
+    </div>
+  );
+};
+
+export default Error;
