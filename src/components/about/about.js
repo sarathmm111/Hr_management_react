c4eb1f4aa07ce07448b2b02e4348b3bba1f2617e
sarath <sarathmm224@gmail.com>
2023-12-19 19:06:40 +0530

    add about componenet

---
+++ src/components/about/about.js
@@ -0,0 +1,34 @@
+import './about.css'
+export default function About(){
+    return(
+    <div id='mainabout'> 
+        
+  <h1>About Our HR Management Website</h1>
+  <p>Welcome to our HR Management platform, where we strive to revolutionize and streamline the way organizations manage
+    their human resources. Our mission is to empower businesses with efficient tools and insights to optimize their
+    workforce, enhance employee experiences, and drive overall organizational success.</p>
+
+  <h3>Our Vision</h3>
+  <p>We envision a workplace where HR processes are seamless, data-driven decisions are the norm, and employees thrive in
+    a supportive and engaging environment. Our HRM website is designed to be the catalyst for positive change in how
+    companies approach human resource management.</p>
+
+  <h3>Key Features</h3>
+
+  <h3>Employee Management</h3>
+  <p>Effortlessly manage employee data, including personal information, attendance, and performance metrics. Streamline
+    onboarding and offboarding processes for a smooth transition at every stage.</p>
+  <h3>Time and Attendance</h3>
+  <p>Keep track of employee work hours, manage leaves, and ensure accurate payroll processing with our time and
+    attendance management features.</p>
+
+  <h3>Training and Development</h3>
+  <p>Invest in the growth of your workforce through our training and development modules. Identify skill gaps, provide
+    targeted training, and nurture talent within your organization.</p>
+
+  <h3>Analytics and Reporting</h3>
+  <p>Make informed decisions with comprehensive analytics and reporting tools. Visualize key HR metrics, monitor trends,
+    and identify areas for improvement.</p>
+    </div>
+    )
+}
