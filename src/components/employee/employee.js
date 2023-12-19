ec579d7094b25ac129091c97b5c1e513f7b17ea0
sarath <sarathmm224@gmail.com>
2023-12-19 19:09:23 +0530

    add employee component

---
+++ src/components/employee/employee.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './employee.css';
+
+export default function Employee() {
+  const [employee, setEmployee] = useState([]);
+  const emplistStyle = {
+    height: '850px', 
+    overflow: 'scroll', 
+  };
+
+  useEffect(() => {
+    fetch('http://localhost:5000/employees')
+      .then(response => response.json())
+      .then(data => setEmployee(data));
+  }, []);
+
+  return (
+    <div id='mainemp'>
+      <br />
+      <h2>EMPLOYEES LIST</h2>
+      
+      <div style={emplistStyle}>
+        {employee.map(data => (
+          <ul>
+            <li>
+              <Link id='link' to={`/employees/${data.id}`} className="navbar-brand">
+                {`${data.firstname} ${data.lastname}`}
+              </Link>
+            </li>
+          </ul>
+        ))}
+      </div>
+    </div>
+  );
+}
