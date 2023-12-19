038195ecd839ea0e258a823ce4ece8a809e524b4
sarath <sarathmm224@gmail.com>
2023-12-19 19:07:49 +0530

    add home component

---
+++ src/components/home/home.js
@@ -0,0 +1,10 @@
+import './home.css'
+export default function Home(){
+    return(
+        <div className="home">
+            <h1 className='title'>" WELCOME TO HAMON HR MANAGEMENT SYSTEM "</h1>
+            <p className='content'>" Empower your business with our comprehensive HR Management Platform. Streamline processes, enhance employee
+             engagement, and drive organizational success. "</p>
+        </div>
+    )
+}
